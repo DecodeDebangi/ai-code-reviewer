d6594b6fc6aef937929bea1a5def9acc7727e7a9
Debangi <debangiofficial@gmail.com>
2025-02-22 15:24:34 +0530

    UI updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,11 +9,11 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [code, setCode] = useState(`function sum(a,b) {
-return a+b
-}`);
+  const [code, setCode] = useState(`Paste your code here...`);
 
-  const [review, setReview] = useState("");
+  const [review, setReview] = useState(
+    "Your code review feedback will appear here afer submission..."
+  );
 
   useEffect(() => {
     prism.highlightAll();
@@ -32,9 +32,17 @@ return a+b
   }
 
   return (
-    <>
-      <main>
-        <div className='left'>
+    <main>
+      <div className='header'>
+        <h1>AI Code Review</h1>
+        <p>
+          Submit your code for an instant, comprehensive review powered by AI.
+        </p>
+      </div>
+
+      <div className='inputWrapper'>
+        <h3>Your Code</h3>
+        <div className='code-editor-box'>
           <div className='code'>
             <Editor
               value={code}
@@ -46,21 +54,48 @@ return a+b
               style={{
                 fontFamily: '"Fira code", "Fira Mono", monospace',
                 fontSize: 12,
-                border: "1px solid #ddd",
-                borderRadius: "5px",
+                borderRadius: "15px",
                 height: "100%",
                 width: "100%",
               }}></Editor>
           </div>
-          <div className='review' onClick={reviewCode}>
-            Review
-          </div>
         </div>
-        <div className='right'>
+        <div className='review-btn' onClick={reviewCode}>
+          Submit for Review
+        </div>
+      </div>
+
+      {/* <div className='code-editor-box'>
+        <div className='code'>
+          <Editor
+            value={code}
+            onValueChange={setCode}
+            highlight={(code) =>
+              prism.highlight(code, prism.languages.javascript, "javascript")
+            }
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+              border: "1px solid #ddd",
+              borderRadius: "5px",
+              height: "100%",
+              width: "100%",
+            }}></Editor>
+        </div>
+        <div className='review' onClick={reviewCode}>
+          Review
+        </div>
+      </div> */}
+
+      <div className='review-wrapper'>
+        <h3>Review output</h3>
+        <hr />
+        <div className='code-review-box'>
           <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
 
